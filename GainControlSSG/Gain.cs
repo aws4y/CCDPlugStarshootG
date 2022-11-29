ed24316897c43f79f374bad18ca84b46f4e8f1ca
Ron Smith <spinlock663@gmail.com>
2022-11-29 07:26:34 -0700

    Added Gain Class for JSON serialization, Driver will DeSerialize into its own version of this class.

---
+++ GainControlSSG/Gain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GainControlSSG
+{
+    public class Gain
+    {
+        [JsonProperty ("max_gain")]
+        public int Max { get; set; }
+        [JsonProperty ("min_gain")]
+        public int Min { get; set; }
+        [JsonProperty ("gain")]
+        public int Value { get; set; }
+    }
+}
